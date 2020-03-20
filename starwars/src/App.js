da5f584f772ec98265f87b98357c5cbaf3ce2662
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-03-20 14:01:02 -0400

    stretch attempted

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import ReactDOM from "react-dom";
 import './App.css';
 import axios from "axios";
 import SpaceBalls from "./components/SpaceBalls";
@@ -40,6 +41,38 @@ useEffect(() => {
 
     </div>
   );
-}
+
+  // const [searchTerm, setSearchTerm] = React.useState("");
+  // const [searchResults, setSearchResults] = React.useState([]);
+  // const handleChange = event => {
+  //    setSearchTerm(event.target.value);
+  //  };
+  // React.useEffect(() => {
+  //    const results = people.filter(person =>
+  //      person.toLowerCase().includes(searchTerm)
+  //    );
+  //    setSearchResults(results);
+  //  }, [searchTerm]);
+ 
+  //  return (
+  //    <div className="Apps">
+  //      <input
+  //        type="text"
+  //        placeholder="Search"
+  //        value={searchTerm}
+  //        onChange={handleChange}
+  //      />
+  //      <ul>
+  //         {searchResults.map(item => (
+  //          <li>{item}</li>
+  //        ))}
+  //      </ul>
+  //    </div>
+  //  );
+ }
+
+
 
 export default App;
+
+
