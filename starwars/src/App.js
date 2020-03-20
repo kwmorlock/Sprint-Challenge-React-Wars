74d28a8c1cd25b52269c0608480e0708ccc9a9d9
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-03-20 11:33:22 -0400

    useEffect started

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,10 @@
-import React { useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [data, setData] = useState([]);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
